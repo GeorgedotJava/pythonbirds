65587e999945975a3c6d39e3ff2e949104289f7d
George <geolucas3@gmail.com>
2020-09-25 16:05:34 -0300

    Atributos nome, sobrenome e idade criados

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,15 @@
 class Pessoa:
-    pass
+    # Método construtor
+    def __init__(self, nome='George', sobrenome='Vieira', idade=26):
+        self.nome = nome
+        self.sobrenome = sobrenome
+        self.idade = idade
+
+
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
+
+
+if __name__ == '__main__':
+    p = Pessoa('Jaide')
+    print(p.nome)
